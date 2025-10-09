ea3ab22b95fc1090a94b2867980dfef881ed967e
Jonas Gahlert <gahlert@zimo.at>
2025-10-09 12:20:56 +0200

    feat: add extract_value

--- include/ztl/bits.hpp
+++ include/ztl/bits.hpp
@@ -36,21 +36,60 @@ constexpr uint32_t make_mask(Ts... bits) {
 /// If the value has more bits than the mask, every bit above the masks MSB is
 /// ignored.
 ///
+/// \tparam Packed  Bitmask bits are packed
 /// \tparam T     Type of bitmask
 /// \tparam U     Type of value
 /// \param  mask  Bitmask
 /// \param  value Value
 /// \return Value shifted to fit bitmask
-template<typename T, typename U>
+template<bool Packed = false,
+         std::unsigned_integral T,
+         std::unsigned_integral U>
 constexpr uint32_t make_value(T mask, U value) {
+  if (Packed) { // Set bits packet together, faster at runtime than iterating
+    return static_cast<uint32_t>((value << std::countr_zero(mask)) & mask);
+  }
   uint32_t retval{};
-  for (auto i{0u}; i < sizeof(uint32_t) * 8u; ++i)
+  for (auto i{0u}; i < sizeof(T) * 8u; ++i)
     // If bit on position is set, shift into retval
     if (mask & (1u << i)) {
       retval |= (value & 1u) << i;
       value >>= 1u;
     }
   return retval;
+}
+
+/// Extract value for bitmask
+///
+/// \details  Takes a <bitmask, bitfield> pair and builds a value from the set
+///           bits of the mask.
+///
+///           E.g. (0b1101, 0b1001) = 0b0101
+///
+///           Everything above the MSB of the bitmask is ignored.
+///
+/// \tparam Packed  Bitmask bits are packed
+/// \tparam T       Type of bitmask
+/// \tparam U       Type of value
+/// \param mask     Bitmask
+/// \param value    Value
+/// \return Extracted value fitting bitmask
+template<bool Packed = false,
+         std::unsigned_integral T,
+         std::unsigned_integral U>
+constexpr uint32_t extract_value(T mask, U value) {
+  if (Packed) { // Set bits packet together, faster at runtime than iterating
+    return static_cast<uint32_t>((value & mask) >> std::countr_zero(mask));
+  } else {
+    uint32_t retval{};
+    // If bit on position is set, shift into retval
+    for (int i{sizeof(T) * 8 - 1}; i >= 0; --i)
+      if (mask & (1u << i)) {
+        retval <<= 1;
+        retval |= (value >> i) & 1u;
+      }
+    return retval;
+  }
 }
 
 } // namespace ztl
--- tests/bits.cpp
+++ tests/bits.cpp
@@ -11,3 +11,41 @@ TEST(bits, make_mask) {
   EXPECT_EQ(ztl::make_mask(30u, 12u, 7u, 1u),
             1u << 30u | 1u << 12u | 1u << 7u | 1u << 1u);
 }
+
+TEST(bits, make_value) {
+  auto const value{0b0101u};
+  auto const mask{0b1101u};
+
+  EXPECT_EQ(ztl::make_value<false>(mask, value), 0b1001u);
+}
+
+TEST(bits, make_value_packed_mask) {
+  auto const value{0b0101u};
+  auto const mask{0b1100u};
+
+  EXPECT_EQ(ztl::make_value<false>(mask, value), 0b100u);
+  EXPECT_EQ(ztl::make_value<true>(mask, value), 0b100u);
+}
+
+TEST(bits, extract_value) {
+  auto const field{0b1001u};
+  auto const mask{0b1101u};
+
+  EXPECT_EQ(ztl::extract_value<false>(mask, field), 0b0101u);
+}
+
+TEST(bits, extract_value_packet_mask) {
+  auto const field{0b1011u};
+  auto const mask{0b1100u};
+
+  EXPECT_EQ(ztl::extract_value<false>(mask, field), 0b10u);
+  EXPECT_EQ(ztl::extract_value<true>(mask, field), 0b10u);
+}
+
+TEST(bits, make_value_then_extract_value) {
+  auto const value{0b0101u};
+  auto const mask{0b1101u};
+
+  EXPECT_EQ(ztl::extract_value<false>(mask, ztl::make_value(mask, value)),
+            value);
+}
