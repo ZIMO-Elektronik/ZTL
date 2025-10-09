ee567d2becb4dbb172a5e9ebff6bad7b3a9c0506
Jonas Gahlert <gahlert@zimo.at>
2025-10-09 16:34:02 +0200

    chore: deprecated make_value and make_mask
    feat: add mask, map_value_to and map_value_from compile-time expressions

--- include/ztl/bits.hpp
+++ include/ztl/bits.hpp
@@ -13,18 +13,41 @@
 #include <concepts>
 #include <cstdint>
 #include <type_traits>
+#include "limits.hpp"
 
 namespace ztl {
+
+/// Checks if a bitmask has only adjacent set bits
+///
+/// \tparam T Type of bitmask
+/// \param t  Bitmask
+/// \retval true  Bitmask contiguous
+/// \retval false Bitmask not contiguous
+template<std::unsigned_integral T>
+constexpr auto is_contiguous_bitmask(T const t) {
+  return (std::countl_zero(t) + std::countr_zero(t) + std::popcount(t)) ==
+         sizeof(T) * 8u;
+}
 
 /// Make mask from bit positions
 ///
 /// \tparam Ts... Type of bit positions
 /// \param  bits  Bit positions
 /// \return Mask for selected bits
+///
+/// \deprecated Use mask<Bits...> instead
 template<std::unsigned_integral... Ts>
-constexpr uint32_t make_mask(Ts... bits) {
+[[deprecated]] constexpr uint32_t make_mask(Ts... bits) {
   return ((1u << bits) | ...);
 }
+
+/// Make mask from bit positions
+///
+/// \tparam Bits  Bit positions
+/// \return Mask for selected bits
+template<std::size_t... Bits>
+constexpr auto mask{
+  static_cast<smallest_unsigned_t<((1u << Bits) | ...)>>(((1u << Bits) | ...))};
 
 /// Make value for bitmask
 ///
@@ -36,21 +59,17 @@ constexpr uint32_t make_mask(Ts... bits) {
 /// If the value has more bits than the mask, every bit above the masks MSB is
 /// ignored.
 ///
-/// \tparam Packed  Bitmask bits are packed
 /// \tparam T     Type of bitmask
 /// \tparam U     Type of value
 /// \param  mask  Bitmask
 /// \param  value Value
 /// \return Value shifted to fit bitmask
-template<bool Packed = false,
-         std::unsigned_integral T,
-         std::unsigned_integral U>
-constexpr uint32_t make_value(T mask, U value) {
-  if (Packed) { // Set bits packet together, faster at runtime than iterating
-    return static_cast<uint32_t>((value << std::countr_zero(mask)) & mask);
-  }
+///
+/// \deprecated Use map_value_to instead
+template<typename T, typename U>
+[[deprecated]] constexpr uint32_t make_value(T mask, U value) {
   uint32_t retval{};
-  for (auto i{0u}; i < sizeof(T) * 8u; ++i)
+  for (auto i{0u}; i < sizeof(uint32_t) * 8u; ++i)
     // If bit on position is set, shift into retval
     if (mask & (1u << i)) {
       retval |= (value & 1u) << i;
@@ -59,37 +78,63 @@ constexpr uint32_t make_value(T mask, U value) {
   return retval;
 }
 
-/// Extract value for bitmask
+/// Map value to bitmask
 ///
-/// \details  Takes a <bitmask, bitfield> pair and builds a value from the set
-///           bits of the mask.
+/// This function takes a <bitmask, value> pair and maps the value to the set
+/// bits of the mask.
+///
+/// E.g. (0b1101, 0b0101) = 0b1001
+///
+/// If the value has more bits than the mask, every bit above the masks MSB is
+/// ignored.
+///
+/// \tparam Mask  Bitmask
+/// \tparam U     Type of value
+/// \param  value Value
+/// \return Mapped value
+template<std::size_t Mask, std::unsigned_integral U>
+constexpr auto map_value_to(U value) {
+  using RetType = smallest_unsigned_t<Mask>;
+  if (is_contiguous_bitmask(Mask)) // Help with optimisation
+    return static_cast<RetType>((value << std::countr_zero(Mask)) & Mask);
+
+  RetType retval{};
+  for (auto i{0u}; i < sizeof(RetType) * 8u; ++i)
+    // If bit on position is set, shift into retval
+    if (Mask & (1u << i)) {
+      retval |= static_cast<RetType>((value & 1u) << i);
+      value >>= 1u;
+    }
+  return retval;
+}
+
+/// Map value from bitmask - Reverts the map_value_to operation
+///
+/// \details  Takes a <bitmask, bitfield> pair and re-maps into a value using
+///           the bitmask
 ///
 ///           E.g. (0b1101, 0b1001) = 0b0101
 ///
 ///           Everything above the MSB of the bitmask is ignored.
 ///
-/// \tparam Packed  Bitmask bits are packed
-/// \tparam T       Type of bitmask
+/// \tparam Mask    Bitmask
 /// \tparam U       Type of value
-/// \param mask     Bitmask
 /// \param value    Value
-/// \return Extracted value fitting bitmask
-template<bool Packed = false,
-         std::unsigned_integral T,
-         std::unsigned_integral U>
-constexpr uint32_t extract_value(T mask, U value) {
-  if (Packed) { // Set bits packet together, faster at runtime than iterating
-    return static_cast<uint32_t>((value & mask) >> std::countr_zero(mask));
-  } else {
-    uint32_t retval{};
-    // If bit on position is set, shift into retval
-    for (int i{sizeof(T) * 8 - 1}; i >= 0; --i)
-      if (mask & (1u << i)) {
-        retval <<= 1;
-        retval |= (value >> i) & 1u;
-      }
-    return retval;
-  }
+/// \return Remapped value
+template<std::size_t Mask, std::unsigned_integral U>
+constexpr auto map_value_from(U value) {
+  using RetType = smallest_unsigned_t<1 << std::popcount(Mask)>;
+  if (is_contiguous_bitmask(Mask)) // Help with optimisation
+    return static_cast<RetType>((value & Mask) >> std::countr_zero(Mask));
+
+  RetType retval{};
+  // If bit on position is set, shift into retval
+  for (int i{sizeof(RetType) * 8 - 1}; i >= 0; --i)
+    if (Mask & (1u << i)) {
+      retval = static_cast<RetType>(retval << 1u);
+      retval |= static_cast<RetType>(static_cast<RetType>(value >> i) & 1u);
+    }
+  return retval;
 }
 
 } // namespace ztl
--- tests/bits.cpp
+++ tests/bits.cpp
@@ -2,8 +2,18 @@
 #include <gtest/gtest.h>
 #include <ztl/bits.hpp>
 
-// Create bit mask
-TEST(bits, make_mask) {
+TEST(bits, mask) {
+  EXPECT_EQ(ztl::mask<0u>, 1u << 0u);
+  EXPECT_EQ(ztl::mask<1u>, 1u << 1u);
+  EXPECT_EQ((ztl::mask<7u, 3u>), 1u << 7u | 1u << 3u);
+  EXPECT_EQ((ztl::mask<3u, 7u>), 1u << 7u | 1u << 3u);
+  EXPECT_EQ((ztl::mask<30u, 12u, 7u, 1u>),
+            1u << 30u | 1u << 12u | 1u << 7u | 1u << 1u);
+}
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+TEST(bits, deprecated_make_mask) {
   EXPECT_EQ(ztl::make_mask(0u), 1u << 0u);
   EXPECT_EQ(ztl::make_mask(1u), 1u << 1u);
   EXPECT_EQ(ztl::make_mask(7u, 3u), 1u << 7u | 1u << 3u);
@@ -11,41 +21,41 @@ TEST(bits, make_mask) {
   EXPECT_EQ(ztl::make_mask(30u, 12u, 7u, 1u),
             1u << 30u | 1u << 12u | 1u << 7u | 1u << 1u);
 }
+#pragma GCC diagnostic pop
 
-TEST(bits, make_value) {
+TEST(bits, map_to_mask) {
   auto const value{0b0101u};
-  auto const mask{0b1101u};
 
-  EXPECT_EQ(ztl::make_value<false>(mask, value), 0b1001u);
+  EXPECT_EQ(ztl::map_value_to<0b1101u>(value), 0b1001u);
 }
 
-TEST(bits, make_value_packed_mask) {
+TEST(bits, map_to_mask_contiguous) {
   auto const value{0b0101u};
-  auto const mask{0b1100u};
 
-  EXPECT_EQ(ztl::make_value<false>(mask, value), 0b100u);
-  EXPECT_EQ(ztl::make_value<true>(mask, value), 0b100u);
+  EXPECT_EQ(ztl::map_value_to<0b1100u>(value), 0b100u);
 }
 
-TEST(bits, extract_value) {
+TEST(bits, map_from_mask) {
   auto const field{0b1001u};
-  auto const mask{0b1101u};
 
-  EXPECT_EQ(ztl::extract_value<false>(mask, field), 0b0101u);
+  EXPECT_EQ(ztl::map_value_from<0b1101>(field), 0b0101u);
 }
 
-TEST(bits, extract_value_packet_mask) {
+TEST(bits, map_from_mask_contiguous) {
   auto const field{0b1011u};
-  auto const mask{0b1100u};
 
-  EXPECT_EQ(ztl::extract_value<false>(mask, field), 0b10u);
-  EXPECT_EQ(ztl::extract_value<true>(mask, field), 0b10u);
+  EXPECT_EQ(ztl::map_value_from<0b1100u>(field), 0b10u);
 }
 
-TEST(bits, make_value_then_extract_value) {
+TEST(bits, map_to_then_from_mask) {
   auto const value{0b0101u};
-  auto const mask{0b1101u};
 
-  EXPECT_EQ(ztl::extract_value<false>(mask, ztl::make_value(mask, value)),
+  EXPECT_EQ(ztl::map_value_from<0b1101u>(ztl::map_value_to<0b1101u>(value)),
             value);
+}
+
+TEST(bits, map_to_with_template_mask) {
+  auto const value{0b0101u};
+
+  EXPECT_EQ((ztl::map_value_to<ztl::mask<3u, 2u, 0u>>(value)), 0b1001u);
 }
